d13d03da36986c664e27a2ec2d1b562262282502
Pascal Herzog <pascal.herzog@students.fhnw.ch>
2022-06-12 19:11:25 +0200

    finish mvp.

--- src/scatterplot.js
+++ src/scatterplot.js
@@ -1,27 +1,22 @@
-// using d3 for convenience
 var main = d3.select("main");
 var scrolly = main.select("#scrolly");
 var figure = scrolly.select("figure");
 var article = scrolly.select("article");
 var step = article.selectAll(".step");
+let heightMod = 0.9;
 
 // initialize the scrollama
 var scroller = scrollama();
 
-// generic window resize listener event
 function handleResize() {
-    // 1. update height of step elements
-    var stepH = Math.floor(window.innerHeight * 0.75);
-    // step.style("height", stepH + "px");
-
-    var figureHeight = window.innerHeight / 2;
+    var stepH = Math.floor(window.innerHeight * heightMod);
+    var figureHeight = window.innerHeight / 1.2;
     var figureMarginTop = (window.innerHeight - figureHeight) / 5;
 
     figure
         .style("height", figureHeight + "px")
         .style("top", figureMarginTop + "px");
 
-    // 3. tell scrollama to update new element dimensions
     scroller.resize();
 }
 
@@ -77,7 +72,6 @@ function handleStepEnter(response) {
             }
             legendAllGames.on("click", function (event){
                 const genre = event.target.getAttribute("data-genre");
-                console.log(genre);
                 selectGenre(genre);
             })
             YSelect.disabled = true;
@@ -121,6 +115,7 @@ function handleStepEnter(response) {
             XSelect.disabled = false;
             YSelect.value = "score";
             YSelect.dispatchEvent(new Event('change'));
+            gameSearch.disabled = false;
             break;
 
     }
@@ -139,8 +134,9 @@ function init() {
     scroller
         .setup({
             step: "#scrolly article .step",
-            offset: 0.8,
-            debug: false
+            offset: 0.6,
+            debug: false,
+            threshold: 1
         })
         .onStepEnter(handleStepEnter);
 }
@@ -148,9 +144,9 @@ init();
 
 
 // create svg canvas
-const margin = {top: 10, right: 10, bottom: 10, left: 50};
-const width = window.innerWidth-300;
-const height = window.innerHeight*0.8;
+const margin = {top: 10, right: 100, bottom: 10, left: 50};
+const width = window.innerWidth*0.8;
+const height = window.innerHeight*0.9;
 
 
 // const canvHeight = 700, canvWidth = 1600;
@@ -162,8 +158,7 @@ const svgAllGames = figure.append("svg")
 const svgGenres = figure.append("svg")
     .attr("width", width*1.2)
     .attr("height", height)
-    .attr("display", "none");
-
+    .attr("display", "block");
 
 
 const xData = [
@@ -171,24 +166,24 @@ const xData = [
         label:"Time to beat main game",
         value:"main_70_noise",
         trueValue:"main_time",
-        axis:"Time to beat the main story in hours"
+        axis:"Main Story, hours, cut off at 71"
     },    {
         label:"Time to beat main game and extras",
         value:"main_extra_time_70_noise",
         trueValue:"main_extra_time",
-        axis:"Time to complete main game and some bonus content in hours"
+        axis:"Main Story + some extra content, hours, cut off at 71"
 
     },    {
         label:"Time to complete everything",
         value:"completion_time_250_noise",
         trueValue:"completion_time_250",
-        axis:"Time to see and do everything a game has to offer in hours"
+        axis:"Time to 100% a game, hours, cut off at 251"
 
     },    {
         label:"Time spent only on bonus content",
         value:"bonus_content_250_noise",
         trueValue:"bonus_content_250",
-        axis:"Time to see and do everything a game has to offer that is not part of the main story in hours"
+        axis:"Time to 100% a game besides main story, hours, cut off at 251"
     }
 ];
 const yData = [
@@ -202,11 +197,12 @@ const yData = [
         value:"initialprice",
         axis:"Price, USD"
     },
-    {
-        label:"Concurrent Users",
-        value:"ccu_5k",
-        axis:"Concurrent players"
-    }
+    //concurrent users looks bad without log scaling, so I removed it entirely.
+    // {
+    //     label:"Concurrent Users",
+    //     value:"ccu_5k",
+    //     axis:"Concurrent players"
+    // }
 ];
 
 let currentGenre = "";
@@ -230,16 +226,32 @@ let legendAllGames;
 
 var data_points;
 
+let divUserControls = figure.append("div")
+    .attr("id", "divUserControls");
 
-d3.select("figure").append("select")
+let divYselect = divUserControls.append("div")
+    .attr("id", "divYselect")
+divYselect.append("label")
+    .attr("id", "forSelectYButton")
+    .html("Y-axis:");
+divYselect.append("select")
     .attr("id", "selectYButton")
-d3.select("figure").append("select")
+
+let divXselect = divUserControls.append("div")
+    .attr("id", "divXselect")
+divXselect.append("label")
+    .attr("id", "forSelectXButton")
+    .html("X-axis:");
+divXselect.append("select")
     .attr("id", "selectXButton")
 
-d3.select("figure").append('input')
+divUserControls.append('input')
     .attr('type','text')
     .attr('id','filterGames')
     .attr('name','filterGames')
+    .attr('placeholder','search games');
+let gameSearch = document.getElementById("filterGames");
+gameSearch.disabled = true;
 
 
 
@@ -286,50 +298,94 @@ const chartAreaGenres = svgGenres.append("g")
     .attr("transform", "translate(" +margin.left + "," + margin.top + ")");
 
 xAxisLabelAllGames = chartAreaAllGames.append("text")
-    .attr("y", height *0.77)
+    .attr("y", height *heightMod*1.05)
     .attr("x", width / 2)
     .attr("dy", "1em")
     .attr("font-family", "sans-serif")
     .attr("text-type", "x-axis-label")
     .style("text-anchor", "middle")
-    .text("Time to beat in hours");
+    .text("Main Story, hours, cut off at 71\n");
 
 xAxisLabelGenres = chartAreaGenres.append("text")
-    .attr("y", height *0.77)
+    .attr("y", height *heightMod*1.05)
     .attr("x", width / 2)
     .attr("dy", "1em")
     .attr("font-family", "sans-serif")
     .attr("text-type", "x-axis-label")
     .style("text-anchor", "middle")
-    .text("Time to beat in hours");
+    .text("Main Story, hours, cut off at 71\n");
 
 
 yAxisLabelAllGames = chartAreaAllGames.append("text")
     .attr("transform", "rotate(-90)")
-    .attr("y", 0 - margin.left)
+    .attr("y", 0 - margin.left*1.1)
     .attr("x",0 - (height / 2))
     .attr("dy", "1em")
     .attr("font-family", "sans-serif")
     .attr("text-type", "y-axis-label")
     .style("text-anchor", "middle")
-    .text("Review Rating in %");
+    .text("Review Rating, %");
 
 yAxisLabelGenres = chartAreaGenres.append("text")
     .attr("transform", "rotate(-90)")
-    .attr("y", 0 - margin.left)
+    .attr("y", 0 - margin.left*1.1)
     .attr("x",0 - (height / 2))
     .attr("dy", "1em")
     .attr("font-family", "sans-serif")
     .attr("text-type", "y-axis-label")
     .style("text-anchor", "middle")
-    .text("Review Rating in %");
+    .text("Review Rating, %");
 
 function selectGenre(genre){
+    gameSearch.value='';
+
+    let rects = legendAllGames.selectAll("rect.legend-item");
+    let texts = legendAllGames.selectAll("text.legend-item");
+
+    console.log(rects);
+    console.log(data_points);
+
     if (genre === currentGenre){
-        data_points.style("display", "block")
+        data_points.style("display", "block");
+        rects.style("opacity", "1");
+        texts.style("opacity", "1");
         currentGenre = "";
     }
     else {
+        rects.filter(function (d) {
+            // if (d.getAttributeNames.includes('data-genre')){
+            //     return false;
+            // }
+            console.log(d);
+            return d === genre;
+        }).style("opacity", "1");
+
+        rects.filter(function (d) {
+            // if (d.getAttributeNames.includes('data-genre')){
+            //     return false;
+            // }
+
+            return d !== genre;
+        }).style("opacity", "0.5");
+
+        texts.filter(function (d) {
+            // if (d.getAttributeNames.includes('data-genre')){
+            //     return false;
+            // }
+
+            return d === genre;
+        }).style("opacity", "1");
+
+        texts.filter(function (d) {
+            // if (d.getAttributeNames.includes('data-genre')){
+            //     return false;
+            // }
+
+            return d !== genre;
+        }).style("opacity", "0.5");
+
+
+
         data_points.filter(function (d) {
             return d['single_genre'] === genre;
         })
@@ -341,6 +397,8 @@ function selectGenre(genre){
             .style("display", "none");
         currentGenre = genre;
     }
+
+
 
     var xSeries = globalData.filter(function (e){
         if (currentGenre !== ""){
@@ -361,6 +419,8 @@ function selectGenre(genre){
     updateTrendline(xSeries, ySeries);
 }
 
+
+
 function createLegend(legendDomain, data) {
     legendAllGames = svgAllGames.append("g")
         .attr("id", "legend")
@@ -376,16 +436,16 @@ function createLegend(legendDomain, data) {
         .attr("width", 20)
         .attr("height", 20)
         .attr("fill", d => colorScale(d))
-        // .attr("stroke", "black")
-        // .attr("stroke-width", "1")
-        .attr("data-genre", d => d);
+        .attr("data-genre", d => d)
+        .attr("class", "legend-item");
 
 
     legend_entryAllGames.append("text")
         .attr("x", 40)
         .attr("y", (d,i) => 30 * i + 25)
         .text(d => d)
-        .attr("data-genre", d => d);
+        .attr("data-genre", d => d)
+        .attr("class", "legend-item");
 
 
     legendAllGames.append("rect")
@@ -394,8 +454,6 @@ function createLegend(legendDomain, data) {
         .attr("width", margin.right - 15)
         .attr("height", legendDomain.length * 30 + 10)
         .attr("fill", "none")
-    // .attr("stroke", "black")
-    // .attr("stroke-width", "1");
 
     const legendGenres = svgGenres.append("g")
         .attr("id", "legend")
@@ -484,7 +542,7 @@ d3.csv("./data/all_steam_games_with_time_data_prepared_for_vis.csv").then(functi
 
     yScale = d3.scaleLinear()
         .domain(yDomain)
-        .rangeRound([height*0.75, 0])
+        .rangeRound([height*heightMod, 0])
         .nice(5);
 
     colorScale = d3.scaleOrdinal(d3.schemeCategory10);
@@ -492,7 +550,7 @@ d3.csv("./data/all_steam_games_with_time_data_prepared_for_vis.csv").then(functi
     let xAxis = d3.axisBottom(xScale);
     drawnXAxis = chartAreaAllGames.append("g")
         .attr("id", "x-axis")
-        .attr("transform", "translate(0, " + height*0.75 + ")")
+        .attr("transform", "translate(0, " + height*heightMod + ")")
         .call(xAxis);
 
     let yAxis = d3.axisLeft(yScale);
@@ -524,24 +582,47 @@ d3.csv("./data/all_steam_games_with_time_data_prepared_for_vis.csv").then(functi
 
     createLegend(legendDomain, data);
 
-    var tooltip = d3.select("body").append("div").classed("tooltip", true);
+    var tooltip = d3.select("body").append("div").classed("tooltip", true).attr("id", "tooltip-games");
     chartAreaAllGames.selectAll("circle").on("mouseover", (event, d) => {
+        tooltip.html(`<div class="tooltip-text"><b>Name:</b> ${d.name}<br/>`
+                + `<b>Price:</b> ${Math.round(d.initialprice * 100) / 100} USD<br/>`
+                + `<b>Main Story:</b> ${Math.round(d.main_time * 100) / 100} hours<br/>`
+                + `<b>Main Story+Extras:</b> ${Math.round(d.main_extra_time * 100) / 100} hours<br/>`
+                + `<b>100%:</b> ${Math.round(d.completion_time * 100) / 100} hours<br/>`
+                + `<b>Concurrent Users:</b> ${Math.round(d.ccu * 100) / 100} players<br/>`
+                + `<b>Score:</b> ${Math.round(d.score * 100) / 100}%<br/></div>`
+                + `<img class="tooltip-image" alt="game image" src="https://howlongtobeat.com${d.pic_url}" width="300">`).style("display", "block");
+
+
         var pos = d3.pointer(event, d);
-        tooltip
-            .style("left", pos[0] + "px")
-            .style("top", pos[1] - 28 + "px")
-            .style("visibility", "visible")
-            .html(`Name: ${d.name}<br/>`
-                + `Price: ${Math.round(d.initialprice * 100) / 100} USD<br/>`
-                + `Main Story Time: ${Math.round(d.main_time * 100) / 100} hours<br/>`
-                + `Main Story+Extras Time: ${Math.round(d.main_extra_time * 100) / 100} hours<br/>`
-                + `100% Time: ${Math.round(d.completion_time * 100) / 100} hours<br/>`
-                + `Concurrent Users: ${Math.round(d.ccu * 100) / 100} players<br/>`
-                + `Score: ${Math.round(d.score * 100) / 100}%<br/>`
-                + `<img alt="game image" src="https://howlongtobeat.com${d.pic_url}" width="300">`);
+        var e = window.event;
+
+        setTimeout(function (){
+
+
+            console.log("--------------------")
+            console.log(document.getElementById("tooltip-games").clientHeight)
+            console.log(e.clientY)
+            console.log(window.innerHeight)
+            if (document.getElementById("tooltip-games").clientHeight > window.innerHeight - e.clientY){
+                console.log("move tooltip");
+                pos[1] -= document.getElementById("tooltip-games").clientHeight - (window.innerHeight-e.clientY);
+            }
+
+
+            tooltip
+                .style("left", pos[0] + "px")
+                .style("top", pos[1] - 28 + "px")
+
+
+        }, 100)
+
+
     })
+
+
         .on("mouseout", (event, d) => {
-            tooltip.style("visibility", "hidden");
+            tooltip.style("display", "none");
         })
         .on("click", (event, d) => {
             let url = "https://store.steampowered.com/app/" + d.appid;
@@ -571,7 +652,7 @@ d3.csv("./data/all_steam_games_with_time_data_prepared_for_vis.csv").then(functi
         yDomain = d3.extent(data, d => Number(d[currYAxis]));
         yScale = d3.scaleLinear()
             .domain(yDomain)
-            .rangeRound([height*0.75, 0])
+            .rangeRound([height*heightMod, 0])
             .nice(5);
 
         yAxis = d3.axisLeft(yScale);
@@ -691,23 +772,6 @@ d3.csv("./data/all_steam_games_with_time_data_prepared_for_vis.csv").then(functi
                     .style("display", "none");
             }
 
-            // var xSeries = data.filter(function (e){
-            //     if (currentGenre !== ""){
-            //         return e["single_genre"] === currentGenre;}
-            //     else {return true;}
-            // })
-            //     .map(function(d) {
-            //         return parseFloat(d[currXAxis]); });
-            //
-            // var ySeries = data.filter(function (e){
-            //     if (currentGenre !== ""){
-            //         return e["single_genre"] === currentGenre;}
-            //     else {return true;}
-            // })
-            //     .map(function(d) {
-            //         return parseFloat(d[currYAxis]); });
-            //
-            // updateTrendline(xSeries, ySeries)
     }
 
 });
@@ -723,7 +787,7 @@ d3.csv("./data/genre_medians.csv").then(function(data) {
 
     let yScale = d3.scaleLinear()
         .domain([0,100])
-        .rangeRound([height*0.75, 0])
+        .rangeRound([height*heightMod, 0])
         .nice(5);
 
     colorScale = d3.scaleOrdinal(d3.schemeCategory10);
@@ -731,7 +795,7 @@ d3.csv("./data/genre_medians.csv").then(function(data) {
     let xAxis = d3.axisBottom(xScale);
     let drawnXAxis = chartAreaGenres.append("g")
         .attr("id", "x-axis")
-        .attr("transform", "translate(0, " + height*0.75 + ")")
+        .attr("transform", "translate(0, " + height*heightMod + ")")
         .call(xAxis);
 
     let yAxis = d3.axisLeft(yScale);
@@ -764,22 +828,26 @@ d3.csv("./data/genre_medians.csv").then(function(data) {
         "MMO"];
     // createLegend(legendDomain, data);
 
-    var tooltip = d3.select("body").append("div").classed("tooltip", true);
+    var tooltip = d3.select("body").append("div").classed("tooltip", true).attr("id", "tooltip-genre");
     chartAreaGenres.selectAll("circle").on("mouseover", (event, d) => {
         var pos = d3.pointer(event, d);
+        console.log(pos);
+        if (event.target.height + pos[1] > window.innerHeight){
+            pos[1]-= (event.target.height + pos[1]) - window.innerHeight;
+        }
         tooltip
             .style("left", pos[0] + "px")
             .style("top", pos[1] - 28 + "px")
-            .style("visibility", "visible")
-            .html(`Name: ${d.single_genre}<br/>`
-                + `Price: ${Math.round(d.initialprice * 100) / 100} USD<br/>`
-                + `Main Story Time: ${Math.round(d.main_time * 100) / 100} hours<br/>`
-                + `Main Story+Extras Time: ${Math.round(d.main_extra_time * 100) / 100} hours<br/>`
-                + `100% Time: ${Math.round(d.completion_time * 100) / 100} hours<br/>`
-                + `Score: ${Math.round(d.score * 100) / 100}%<br/>`);
+            .style("display", "block")
+            .html(`<div class="tooltip-text"><b>Name:</b> ${d.single_genre}<br/>`
+                + `<b>Price:</b> ${Math.round(d.initialprice * 100) / 100} USD<br/>`
+                + `<b>Main Story:</b> ${Math.round(d.main_time * 100) / 100} hours<br/>`
+                + `<b>Main Story+Extras:</b> ${Math.round(d.main_extra_time * 100) / 100} hours<br/>`
+                + `<b>100%:</b> ${Math.round(d.completion_time * 100) / 100} hours<br/>`
+                + `<b>Score:</b> ${Math.round(d.score * 100) / 100}%<br/></div>`);
     })
         .on("mouseout", (event, d) => {
-            tooltip.style("visibility", "hidden");
+            tooltip.style("display", "none");
         });
 
 });
@@ -859,7 +927,6 @@ function leastSquares(xSeries, ySeries) {
 
     return [slope, intercept, rSquare];
 }
-
 
 window.onresize = function(){ location.reload(); }
 history.scrollRestoration = 'manual';
